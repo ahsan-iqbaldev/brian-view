d580c36986e6d4c3f44e8d6105b534322f65f67c
MrAhsan5 <150703987+codesbyahsan@users.noreply.github.com>
2024-03-01 01:37:57 +0500

    handle login page UI

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="p-0 w-[80px] mt-0.2 max-h-[80px] inline-block">
+        <img src="/images/logo.svg" alt="ahsan" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Login.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <section className="overflow-hidden flex flex-col text-center h-screen">
+      <div className="mb-48 w-full relative min-h-full box-border flex justify-center items-center flex-col p-20 login-background_image">
+        <div className=" max-w-[650px] flex flex-col w-full">
+          <img
+            src="/images/cta-logo-one.svg"
+            className="mb-5 w-full max-w-[600px] min-h-1 block"
+            alt="logo"
+          />
+          <button className="primary_btn">GET ALL THERE</button>
+          <p className="desciption">Get Premier Access to Raya and the Last Dragon for an additional fee the price of Disney+ and The Disney Bundle will increse by $1 </p>
+          <img
+            src="/images/cta-logo-two.png"
+            className="mt-4 mb-8 max-w-[600px] inline-block w-full"
+            alt="logo"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
